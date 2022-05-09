949e2bfd26f13145b126e618b7580c839cb70777
George MacKerron <george@mackerron.co.uk>
2022-05-09 09:21:13 +0100

    Updated year in copyright notices, plus tiny tweaks in cli.ts

--- src/db/shortcuts.ts
+++ src/db/shortcuts.ts
@@ -1,6 +1,6 @@
 /*
 Zapatos: https://jawj.github.io/zapatos/
-Copyright (C) 2020 - 2021 George MacKerron
+Copyright (C) 2020 - 2022 George MacKerron
 Released under the MIT licence: see LICENCE file
 */
 
--- src/generate/cli.ts
+++ src/generate/cli.ts
@@ -3,7 +3,7 @@
 
 /*
 Zapatos: https://jawj.github.io/zapatos/
-Copyright (C) 2020 - 2021 George MacKerron
+Copyright (C) 2020 - 2022 George MacKerron
 Released under the MIT licence: see LICENCE file
 */
 
@@ -13,19 +13,24 @@ import type { Config } from './config';
 
 
 const recursivelyInterpolateEnvVars = (obj: any): any =>
+  // string? => do the interpolation
   typeof obj === 'string' ?
     obj.replace(/\{\{\s*([^}\s]+)\s*\}\}/g, (_0, name) => {
       const e = process.env[name];
       if (e === undefined) throw new Error(`Environment variable '${name}' is not set`);
       return e;
     }) :
+    // array? => recurse over its items
     Array.isArray(obj) ?
       obj.map(item => recursivelyInterpolateEnvVars(item)) :
+      // object? => recurse over its values (but don't touch the keys)
       typeof obj === 'object' ?
         Object.keys(obj).reduce<any>((memo, key) => {
           memo[key] = recursivelyInterpolateEnvVars(obj[key]);
           return memo;
-        }, {}) : obj;
+        }, {}) :
+        // anything else (e.g. number)? => pass right through
+        obj;
 
 void (async () => {
   const
@@ -36,15 +41,17 @@ void (async () => {
   let fileConfig;
   try {
     fileConfig = recursivelyInterpolateEnvVars(JSON.parse(configJSON));
+
   } catch (err: any) {
-    throw new Error(`If present, zapatosconfig.ts must be a valid JSON file: ${err.message}`);
+    throw new Error(`If present, zapatosconfig.ts must be a valid JSON file, and all referenced environment variables must exist: ${err.message}`);
   }
 
   let argsConfig;
   try {
     argsConfig = recursivelyInterpolateEnvVars(JSON.parse(argsJSON));
+
   } catch (err: any) {
-    throw new Error(`If present, the argument to Zapatos must be valid JSON: ${err.message}`);
+    throw new Error(`If present, the argument to Zapatos must be valid JSON, and all referenced environment variables must exist: ${err.message}`);
   }
 
   await generate({ ...fileConfig, ...argsConfig } as Config);
